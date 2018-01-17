12e02be9eab15f50e065f7efe48ab684ea29b3b3
oztrung4500 <34942790+oztrung4500@users.noreply.github.com>
2018-01-17 16:15:00 +0700

    Add files via upload

---
+++ polis.js
@@ -0,0 +1,8 @@
+﻿$("#pp-show").click(function () {
+    $("body").append('<style id="p1">#pp-show{display:none!important}.PopularPosts ul li:nth-child(1n+6),#pp-hide{display:block !important}</style>');
+    $("#p2").remove()
+});
+$("#pp-hide").click(function () {
+    $("body").append('<style id="p2">.PopularPosts ul li:nth-child(1n+6),#pp-hide{display:none!important}#pp-show{display:block!important}</style>');
+    $("#p1").remove()
+});
